abf2edd17c2851f033dcadb6fae4169c7cf5550f
ClaraPrimeJohn <143156493+ClaraPrimeJohn@users.noreply.github.com>
2023-11-06 19:44:32 +0800

    Add files via upload

--- Google Sheets.js
+++ Google Sheets.js
@@ -1,26 +1,25 @@
 // form
-const form = document.querySelector('#form');
-const submitButton = document.querySelector('#submit');
-const scriptURL = 'https://script.google.com/macros/s/AKfycbxfe45dCRYOowxnKUyL4yzt8KiepSSXHanzEUwkamgA8c7KAcrWg7PM8o9itqyGw-t5xA/exec';
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxfe45dCRYOowxnKUyL4yzt8KiepSSXHanzEUwkamgA8c7KAcrWg7PM8o9itqyGw-t5xA/exec'
+
+const form = document.forms['contact-form']
 
 form.addEventListener('submit', e => {
-  submitButton.disabled = true
-  e.preventDefault();
-  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-  .then(response => {Swal.fire({
-    position: "center",
-    icon: "success",
-    title: "Message Succesfully Sent, Thankyou for getting in touch!",
-    showConfirmButton: false,
-    timer: 2500
-  })
+ e.preventDefault()
+ fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+ .then(response => Swal.fire({
+  position: "top-end",
+  icon: "success",
+  title: "Thankyou for getting in touch with me!",
+  showConfirmButton: false,
+  timer: 1500
+}))
+ .then(() => { window.location.reload(); })
+ .catch(error => Swal.fire({
+  icon: "error",
+  title: "Oops...",
+  text: "Something went wrong!",
+  footer: '<a href="#">Why do I have this issue?</a>'
+}))
 })
-  .then(() => { window.location.reload(); })
-  .catch(error => {Swal.fire({
-    icon: "error",
-    title: "Oops...",
-    text: "Something went wrong!",
-    footer: '<a href="#">Why do I have this issue?</a>'
-  })})
-})
+
 
