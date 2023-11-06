ab309fdb79477bdb5c9dcbf32c9ca81d25a805fe
ClaraPrimeJohn <143156493+ClaraPrimeJohn@users.noreply.github.com>
2023-11-06 18:05:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -188,4 +188,28 @@ themeToggle.addEventListener("change", () => {
   }
 });
 
+// form
+const scriptURL = 'https://script.google.com/macros/s/AKfycbxfe45dCRYOowxnKUyL4yzt8KiepSSXHanzEUwkamgA8c7KAcrWg7PM8o9itqyGw-t5xA/exec'
+
+const form = document.forms['contact-form']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => Swal.fire({
+    position: "top",
+    icon: "success",
+    title: "Message Succesfully Sent",
+    showConfirmButton: false,
+    timer: 1500
+  }))
+  .then(() => { window.location.reload(); })
+  .catch(error => Swal.fire({
+    icon: "error",
+    title: "Oops...",
+    text: "Something went wrong!",
+    footer: '<a href="#">Why do I have this issue?</a>'
+  }))
+})
+
 
